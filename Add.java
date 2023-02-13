ab6cc3b595576ea305d8de84cdd0927ab33c3a42
mahanteshm94 <mahantesh0917@gmail.com>
2023-02-13 17:47:37 +0530

    msg

---
+++ Add.java
@@ -0,0 +1,50 @@
+class Add
+{
+  static void count()
+
+  {
+      
+    for(int i=1;i<=20;i++)
+   {
+        if(i%2==0)
+	   {
+
+         System.out.println(i);
+
+	   }
+
+	  else if(i%3==0)
+        { 
+  
+          System.out.println(i);
+	  
+	  }
+      }
+   }
+  
+public static void main(String[] args)
+
+   {
+   
+   
+   count();
+   
+   }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
