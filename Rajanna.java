9361c1810401c4390a6d6cf4f81847cb9365ff4a
mahanteshm94 <mahantesh0917@gmail.com>
2023-02-13 17:51:14 +0530

    msg

---
+++ Rajanna.java
@@ -0,0 +1,27 @@
+class Rajanna
+{
+static void sum(int a, int b)
+	{
+		int c=a+b;
+
+System.out.println(c);
+	}
+
+public static void main(String[] args)
+
+	{
+		sum(10,20)
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+}
